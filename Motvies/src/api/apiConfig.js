d51984a4fcc4b503749604a08ef828758ec5c7c0
Adnan Haider <118106335+AdnanMehr8@users.noreply.github.com>
2024-04-08 02:31:37 +0500

    Update apiConfig.js

--- Motvies/src/api/apiConfig.js
+++ Motvies/src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '7bd318f0bbf66eb56a76f558afb813d7',
+    apiKey: 'Your_Api_Key',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
