b8cb2b796770732eb2fad993b865aff82e91d654
Adnan Haider <118106335+AdnanMehr8@users.noreply.github.com>
2024-10-09 16:50:33 +0500

    Update Home.jsx
    Added more types

--- Motvies/src/pages/Home.jsx
+++ Motvies/src/pages/Home.jsx
@@ -1,5 +1,65 @@
+// import React from 'react';
+// import {Link} from 'react-router-dom';
+
+// import { OutlineButton } from '../components/button/Button';
+// import HeroSlide from '../components/hero slide/HeroSlide';
+// import MovieList from '../components/movie-list/MovieList';
+
+// import { category, movieType, tvType } from '../api/tmdbApi';
+
+// const Home = () => {
+//   return (
+//     <>
+//       <HeroSlide/>
+//       <div className="container">
+//         <div className="section mb-3">
+//           <div className="section__header mb-2">
+//             <h2>Trending Movies</h2>
+//             <Link to= "/movie">
+//               <OutlineButton className="small">View more</OutlineButton>
+//             </Link>
+//           </div>
+//           <MovieList category={category.movie} type={movieType.popular}/>
+//         </div>
+
+//         <div className="section mb-3">
+//           <div className="section__header mb-2">
+//             <h2>Top Rated Movies</h2>
+//             <Link to= "/movie">
+//               <OutlineButton className="small">View more</OutlineButton>
+//             </Link>
+//           </div>
+//           <MovieList category={category.movie} type={movieType.top_rated}/>
+//         </div>
+
+//         <div className="section mb-3">
+//           <div className="section__header mb-2">
+//             <h2>Trending TV</h2>
+//             <Link to= "/tv">
+//               <OutlineButton className="small">View more</OutlineButton>
+//             </Link>
+//           </div>
+//           <MovieList category={category.tv} type={tvType.popular}/>
+//         </div>
+
+//         <div className="section mb-3">
+//           <div className="section__header mb-2">
+//             <h2>Top Rated TV</h2>
+//             <Link to= "/tv">
+//               <OutlineButton className="small">View more</OutlineButton>
+//             </Link>
+//           </div>
+//           <MovieList category={category.tv} type={tvType.top_rated}/>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Home;
+
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { OutlineButton } from '../components/button/Button';
 import HeroSlide from '../components/hero slide/HeroSlide';
@@ -12,6 +72,7 @@ const Home = () => {
     <>
       <HeroSlide/>
       <div className="container">
+        {/* Trending Movies Section */}
         <div className="section mb-3">
           <div className="section__header mb-2">
             <h2>Trending Movies</h2>
@@ -22,6 +83,7 @@ const Home = () => {
           <MovieList category={category.movie} type={movieType.popular}/>
         </div>
 
+        {/* Top Rated Movies Section */}
         <div className="section mb-3">
           <div className="section__header mb-2">
             <h2>Top Rated Movies</h2>
@@ -32,6 +94,29 @@ const Home = () => {
           <MovieList category={category.movie} type={movieType.top_rated}/>
         </div>
 
+        {/* Now Playing Movies Section */}
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Now Playing Movies</h2>
+            <Link to= "/movie">
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.movie} type={movieType.now_playing}/>
+        </div>
+
+        {/* Latest Movies Section */}
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Latest Movies</h2>
+            <Link to= "/movie">
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.movie} type={movieType.latest}/>
+        </div>
+
+        {/* Trending TV Shows Section */}
         <div className="section mb-3">
           <div className="section__header mb-2">
             <h2>Trending TV</h2>
@@ -42,6 +127,7 @@ const Home = () => {
           <MovieList category={category.tv} type={tvType.popular}/>
         </div>
 
+        {/* Top Rated TV Shows Section */}
         <div className="section mb-3">
           <div className="section__header mb-2">
             <h2>Top Rated TV</h2>
@@ -50,6 +136,28 @@ const Home = () => {
             </Link>
           </div>
           <MovieList category={category.tv} type={tvType.top_rated}/>
+        </div>
+
+        {/* Airing Today TV Shows Section */}
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Airing Today</h2>
+            <Link to= "/tv">
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.airing_today}/>
+        </div>
+
+        {/* Latest TV Shows Section */}
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Latest TV Shows</h2>
+            <Link to= "/tv">
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.latest}/>
         </div>
       </div>
     </>
